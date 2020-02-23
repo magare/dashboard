dec633ae1e7c263544ae8419206eebf62f59ea68
vilas <vilas@numniolabs.com>
2020-02-23 20:51:36 +0530

    Updates

---
+++ precache-manifest.170c6f237948058080ee6ef08063cb09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f802849d4f569775d0048ce5e0448e",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "fd51ff6cc2f7ff119cee",
+    "url": "/dashboard/static/css/main.f30b9a49.chunk.css"
+  },
+  {
+    "revision": "50e986f1b946271f0f9b",
+    "url": "/dashboard/static/js/2.63059c06.chunk.js"
+  },
+  {
+    "revision": "bc1606c6db11c396205f31e2fe0b9d0c",
+    "url": "/dashboard/static/js/2.63059c06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd51ff6cc2f7ff119cee",
+    "url": "/dashboard/static/js/main.451c534a.chunk.js"
+  },
+  {
+    "revision": "a2eb365f0d164ac61c5b",
+    "url": "/dashboard/static/js/runtime-main.823d2d5c.js"
+  }
+]);
